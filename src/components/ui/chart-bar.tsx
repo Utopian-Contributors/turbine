aad89a9554debc46f464f4907e781f518f2128e6
Ludwig Schubert <ludwigschubi1@gmail.com>
2025-11-19 18:32:11 -0800

    feat: add version chart

---
+++ src/components/ui/chart-bar.tsx
@@ -0,0 +1,85 @@
+'use client'
+
+import { Bar, BarChart, LabelList, XAxis, YAxis } from 'recharts'
+
+import { cn } from '@/lib/utils'
+import { filesize } from 'filesize'
+import { ZapIcon } from 'lucide-react'
+import type { ChartConfig } from './chart'
+import { ChartContainer, ChartTooltip, ChartTooltipContent } from './chart'
+
+export const description = 'A bar chart with a custom label'
+
+export interface ChartData {
+  label: string
+  value: number
+  formattedValue: string
+}
+
+export const ChartBarLabelCustom: React.FC<{
+  label: string
+  description: string
+  data: ChartData[]
+  config: ChartConfig
+  classname?: string
+}> = ({ label, description, data, config, classname }) => {
+  return (
+    <div className={cn('border rounded-xl space-y-4 p-4', classname)}>
+      <div>
+        <h1 className="text-lg">{label}</h1>
+        <p className="text-muted-foreground">{description}</p>
+      </div>
+      <ChartContainer config={config}>
+        <BarChart
+          accessibilityLayer
+          data={data}
+          layout="vertical"
+          margin={{
+            right: 80,
+          }}
+        >
+          <YAxis
+            dataKey="label"
+            type="category"
+            tickLine={false}
+            tickMargin={10}
+            axisLine={false}
+          />
+          <XAxis dataKey="value" type="number" hide />
+          <ChartTooltip
+            cursor={false}
+            content={
+              <ChartTooltipContent
+                indicator="line"
+                formatter={(value) => (
+                  <div className="flex gap-2 items-center">
+                    <ZapIcon width={20} className='text-green-600'/>
+                    <div className="flex flex-col">
+                      <p className="text-muted-foreground">Bandwidth</p>
+                      <p>{filesize(value as number)}</p>
+                    </div>
+                  </div>
+                )}
+              />
+            }
+          />
+          <Bar
+            dataKey="value"
+            layout="vertical"
+            fill="var(--color-value)"
+            radius={4}
+            height={40}
+          >
+            <LabelList
+              dataKey="formattedValue"
+              position="right"
+              offset={8}
+              className="fill-foreground"
+              fontSize={12}
+            />
+          </Bar>
+        </BarChart>
+      </ChartContainer>
+    </div>
+  )
+}
--- src/pages/LibraryPage.tsx
+++ src/pages/LibraryPage.tsx
@@ -2,14 +2,17 @@ import Search from '@/components/blocks/search'
 import Bandwidth from '@/components/Library/Bandwidth'
 import Downloads from '@/components/Library/Downloads'
 import Versions from '@/components/Library/Versions'
+import { ChartBarLabelCustom } from '@/components/ui/chart-bar'
 import { Icons } from '@/components/ui/icons'
 import { ScriptCopyBtn } from '@/components/ui/script-copy-button'
 import { Separator } from '@/components/ui/separator'
 import { useFetchLibrary } from '@/hooks/useFetchLibrary'
 import { useSearch } from '@/hooks/useSearch'
 import { useVersions } from '@/hooks/useVersions'
+import { useVersionStats } from '@/hooks/useVersionStats'
 import { Link } from '@radix-ui/themes'
-import { Globe } from 'lucide-react'
+import { filesize } from 'filesize'
+import { Globe, PackageIcon } from 'lucide-react'
 import moment from 'moment'
 import { useEffect, useState } from 'react'
 import { useParams } from 'react-router'
@@ -20,15 +23,19 @@ interface LibraryPageProps {}
 const LibraryPage: React.FC<LibraryPageProps> = () => {
   const params = useParams<{ name: string }>()
   const { fetchLibrary, library, loading } = useFetchLibrary()
+  const { versionStats, fetchVersionStats } = useVersionStats()
   const versions = useVersions(library?.versions || {})
   const [selectedVersion, setSelectedVersion] = useState<string | null>()
   const { search } = useSearch()
 
   useEffect(() => {
-    fetchLibrary(params.name || '').then((lib) => {
-      setSelectedVersion(lib['dist-tags'].latest)
-    })
-  }, [fetchLibrary, params.name])
+    if (params.name && !library) {
+      fetchLibrary(params.name || '').then((lib) => {
+        setSelectedVersion(lib['dist-tags'].latest)
+        fetchVersionStats(params.name || '')
+      })
+    }
+  }, [fetchLibrary, fetchVersionStats, library, params.name])
 
   if (loading) {
     return <div>Loading...</div>
@@ -47,15 +54,18 @@ const LibraryPage: React.FC<LibraryPageProps> = () => {
       />
       <div className="grid grid-cols-3 mt-6">
         <div className="col-span-2">
-          <div className="flex flex-col gap-2">
+          <div className="flex flex-col gap-2 p-2">
             <div className="w-full flex justify-between items-center">
-              <h1 className="text-4xl">{library.name}</h1>
+              <div className="flex gap-2 items-end">
+                <PackageIcon className="fill-lime-400" width={32} height={32} />
+                <h3 className="text-4xl">{library.name}</h3>
+              </div>
               <div className="flex gap-2">
                 <Link href={library.homepage} target="_blank">
                   <Globe className="h-5 w-5 text-gray-200 hover:text-gray-500 cursor-pointer" />
                 </Link>
                 <Link
-                  href={library.repository.url.replace('git:', 'https:')}
+                  href={library.repository?.url.replace('git+', '')}
                   target="_blank"
                 >
                   <Icons.gitHub className="h-5 w-5 text-gray-200 hover:text-gray-500 cursor-pointer" />
@@ -78,10 +88,31 @@ const LibraryPage: React.FC<LibraryPageProps> = () => {
               </span>
             </div>
           </div>
-          <Separator className="my-4" />
+          <Separator className="my-6" />
+          {versionStats ? (
+            <ChartBarLabelCustom
+              classname="mt-4"
+              label="Popular versions"
+              description="Top 10 versions by bandwidth usage in the last week"
+              data={versionStats.map((stat) => ({
+                label: stat.version,
+                value: stat.bandwidth,
+                formattedValue: filesize(stat.bandwidth),
+              }))}
+              config={{
+                value: {
+                  label: 'Bandwidth',
+                  color: 'gray',
+                },
+                label: {
+                  color: 'var(--background)',
+                },
+              }}
+            />
+          ) : null}
         </div>
         <div className="col-span-1 gap-4 flex flex-col">
-          <div className="hidden md:flex flex-col items-start gap-4 mx-8 p-4 border rounded-xl">
+          <div className="border rounded-xl flex flex-col gap-2 mx-8 p-4">
             <Versions
               versions={versions}
               selected={selectedVersion}
@@ -100,7 +131,7 @@ const LibraryPage: React.FC<LibraryPageProps> = () => {
               }}
             />
           </div>
-          <div className="max-w-[16rem] flex flex-col gap-2 mx-8 p-4 border rounded-xl">
+          <div className="border rounded-xl max-w-[16rem] flex flex-col gap-2 ml-8 p-4 border rounded-xl">
             <Downloads library={library.name} />
             <Bandwidth library={library.name} />
           </div>
