1eb557b87cb65584d53049337e25284c2cdd276b
Ludwig Schubert <ludwigschubi1@gmail.com>
2025-12-29 23:49:52 +0100

    fix: phantom login

--- src/App.tsx
+++ src/App.tsx
@@ -98,7 +98,7 @@ function App() {
           AddressType.sui,
         ],
         authOptions: {
-          redirectUrl: 'http://turbine.utopian.build/wallet/', // Must be whitelisted in Phantom Portal
+          redirectUrl: 'https://turbine.utopian.build/wallet/', // Must be whitelisted in Phantom Portal
         },
       }}
       theme={lightTheme}
